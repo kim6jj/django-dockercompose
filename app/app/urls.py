5afa7a6c9ab6127fbb9488e30b1be412efc04e43
JJ Kim <jjkim@JJs-MacBook-Pro.local>
2022-02-04 11:05:20 -0800

    first test site

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+import core.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', core.views.home, name='home'),
 ]
 
 if settings.DEBUG:
